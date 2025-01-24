38f225dd719c6da5d5c77a24f996d2b8232adf67
masumkhan081 <masumkhan081@gmail.com>
2025-01-24 23:55:05 +0600

    Test commit

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,11 +1,11 @@
 "use client"
 import React from 'react'
-import { useRouter } from 'next/navigation';
-import { useState, useEffect } from 'react';
+// import { useRouter } from 'next/navigation';
+// import { useState } from 'react';
 
 export default function page() {
-    const router = useRouter();
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    // const router = useRouter();
+    // const [isLoggedIn, setIsLoggedIn] = useState(false);
     return (
         <div>admin-age</div>
     )
